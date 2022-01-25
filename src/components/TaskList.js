5d10a697930610315b50b1523950238a0cffff67
shamala suresh babu <suresh.shamala@in.pega.com>
2022-01-26 01:53:58 +0530

    second commit

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -3,7 +3,15 @@ import PropTypes from "prop-types";
 
 import Task from "./Task";
 
-export default function TaskList({ loading, tasks, onPinTask, onArchiveTask }) {
+import { useDispatch, useSelector } from "react-redux";
+import { updateTaskState } from "../lib/store";
+
+export default function PureTaskList({
+  loading,
+  tasks,
+  onPinTask,
+  onArchiveTask,
+}) {
   const events = {
     onPinTask,
     onArchiveTask,
@@ -57,7 +65,7 @@ export default function TaskList({ loading, tasks, onPinTask, onArchiveTask }) {
   );
 }
 
-TaskList.propTypes = {
+PureTaskList.propTypes = {
   /** Checks if it's in loading state */
   loading: PropTypes.bool,
   /** The list of tasks */
@@ -68,6 +76,33 @@ TaskList.propTypes = {
   onArchiveTask: PropTypes.func,
 };
 
-TaskList.defaultProps = {
+PureTaskList.defaultProps = {
   loading: false,
 };
+
+export function TaskList() {
+  // We're retrieving our state from the store
+  const tasks = useSelector((state) => state.tasks);
+  // We're defining an variable to handle dispatching the actions back to the store
+  const dispatch = useDispatch();
+
+  const pinTask = (value) => {
+    // We're dispatching the Pinned event back to our store
+    dispatch(updateTaskState({ id: value, newTaskState: "TASK_PINNED" }));
+  };
+  const archiveTask = (value) => {
+    // We're dispatching the Archive event back to our store
+    dispatch(updateTaskState({ id: value, newTaskState: "TASK_ARCHIVED" }));
+  };
+
+  const filteredTasks = tasks.filter(
+    (t) => t.state === "TASK_INBOX" || t.state === "TASK_PINNED"
+  );
+  return (
+    <PureTaskList
+      tasks={filteredTasks}
+      onPinTask={(task) => pinTask(task)}
+      onArchiveTask={(task) => archiveTask(task)}
+    />
+  );
+}
